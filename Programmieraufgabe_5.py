cf609e9324990e3e08c483ea6ace063df5633c9d
Hanno Seibod <hannoseibod@outlook.com>
2021-01-15 14:56:11 +0100

    Upload Data

---
+++ Programmieraufgabe_5.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Programmieraufgabe 5: QR-Zerlegung
+
+# In[1]:
+
+
+import numpy as np
+
+
+# ## a)
+
+# In[2]:
+
+
+def QR(A):
+    ## Ihr Code hier
+    pass
+
+
+# ## b)
+
+# In[3]:
+
+
+A = np.array([[2., 0., 1.],
+              [0., 2., 7.],
+              [0., 6., 0.],
+              [0., 3., 0.],
+              [np.sqrt(12.), 0., np.sqrt(3)]])
+
+## Ihr Code hier
+
+
+# ## c)
+
+# In[4]:
+
+
+def QT_Produkt(b, M, tau):
+    ## Ihr Code hier
+    pass
+
+def R_Loesung(c, M):
+    ## Ihr Code hier
+    pass
+
+
+# ## d)
+
+# In[5]:
+
+
+A = np.array([[-1., 1., -1.],
+              [2., 4., 5.],
+              [-2., -1., 1.]])
+b = np.array([1., 1., 1.])
+
+## Ihr Code hier
+
+
+# ## e)
+
+# In[6]:
+
+
+A = np.array([[-1., 1.],
+              [2., 4.],
+              [-2., -1.]])
+b = np.array([1., 1., 2.])
+
+## Ihr Code hier
+
+
+# In[ ]:
+
+
+
+
